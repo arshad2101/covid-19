6edf5c240e94359e43d0917ca83839fd84f967f4
Mohd Arshad <arshad@cyclops.nic.uoregon.edu>
2020-04-06 22:24:59 -0700

    Modified for threshhold

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
 from flask import Flask, url_for, send_from_directory, request,render_template
 import logging, os, json
 from werkzeug import secure_filename
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, ZeroPadding2D, Flatten, Activation
+from keras.models import Sequential, Model
 import os 
+import cv2
+import shutil
+import base64
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 dir_path = os.path.dirname(os.path.realpath(__file__))
 print(dir_path)
@@ -65,24 +70,32 @@ def api_root():
     
     app.logger.info(PROJECT_HOME)
     if request.method == 'POST' and request.files['image']:
-    	#print(request.is_json)
-    	#content = request.get_json()
-    	print(request.headers)
-    	params = request.values
-    	names=params.get('name')
-    	ids=params.get('id')
-    	app.logger.info(app.config['UPLOAD_FOLDER'])
-    	img = request.files['image']
-    	img_name = secure_filename(img.filename)
-    	create_new_folder(app.config['UPLOAD_FOLDER'])
-    	saved_path = os.path.join(app.config['UPLOAD_FOLDER'], img_name)
-    	app.logger.info("saving {}".format(saved_path))
-    	img.save(saved_path)
-    	predicted_classes = predict(img_name,names,ids)
-    	#return send_from_directory(app.config['UPLOAD_FOLDER'],img_name, as_attachment=True)
-    	return predicted_classes
+        #print(request.is_json)
+        #content = request.get_json()
+        print(request.headers)
+        params = request.values
+        names=params.get('name')
+        ids=params.get('id')
+        app.logger.info(app.config['UPLOAD_FOLDER'])
+        img = request.files['image']
+        img_name = secure_filename(img.filename)
+        create_new_folder(app.config['UPLOAD_FOLDER'])
+        saved_path = os.path.join(app.config['UPLOAD_FOLDER'], img_name)
+        app.logger.info("saving {}".format(saved_path))
+        img.save(saved_path)
+        isAnamoly,_mse , threshold = anamolyDetector(saved_path)
+        if isAnamoly:
+            base64img = ''
+            with open('uploads/test/'+img_name,mode = 'rb') as image_file:
+                    img = image_file.read()
+                    base64img = base64.encodebytes(img).decode("utf-8")
+            shutil.rmtree('uploads/test/')
+            return '<html><title></title><body><h1><center>Uploaded Image is not valid Lungs X-Ray</center></h1><br/><h3><center>Threshold set: '+str(threshold)+' Error calculated:'  +str(_mse)+ '</center></h3><br/><center><img src="data:image/jpeg;base64,'+base64img+'"></<center></body></html>'
+        predicted_classes = predict(img_name,names,ids,_mse , threshold )
+        #return send_from_directory(app.config['UPLOAD_FOLDER'],img_name, as_attachment=True)
+        return predicted_classes
     else:
-    	return "<html><title></title><body><h1><center>Please upload an image</center></h1></body></html>"
+        return "<html><title></title><body><h1><center>Please upload an image</center></h1></body></html>"
 
 import tensorflow as tf
 session = tf.Session(graph=tf.Graph())
@@ -112,7 +125,7 @@ def test_datagenerator():
 
     return train_generator'''
     
-def predict(img_name,names,ids):
+def predict(img_name,names,ids,_mse , threshold ):
     global sess
     global graph
    # print('loading trained model...')
@@ -155,8 +168,7 @@ def predict(img_name,names,ids):
     #print("Test Accuracy = {0:.2f}%".format(len(correct_predictions)*100/test_generator.samples))
     print("Predicted in {0:.3f} minutes!".format(prediction_time/60))
     #return json.dumps(data, indent=4)
-    import shutil
-    import base64
+    
     base64img = ''
     with open('uploads/test/'+img_name,mode = 'rb') as image_file:
         img = image_file.read()
@@ -174,14 +186,72 @@ def predict(img_name,names,ids):
     dictOfWords = dict(zipbObj)
     
     
-    strhtml='<html><head><style>  body{background-color:RosyBrown;}#customers {font-family: "Lucida Console", Monaco, monospace;border-collapse: collapse;border-radius: 2em;overflow: hidden;width:80%;height:45%;margin-top: 150px;margin-right: 150px;margin-left:150px;}#customers td, #customers th {border: 1px solid #ddd;padding: 8px;}#customers tr:nth-child(even){background-color: LavenderBlush;}#customers tr:hover {background-color: #ddd;}#customers th {padding-top: 12px;padding-bottom: 12px;text-align: left;background-color: DeepSkyBlue;color: white;}</style></head><body><p></p><table id="customers" ><tr><th>PATIENT_NAME</th><th>PATIENT_ID</th><th>IMAGE_NAME</th><th>PREDICTED_CLASS</th><th>PREDICTIONS</th><th>IMAGE</th></tr><tr><td>'+names+'</td><td>'+ids+'</td><td>'+a[5:]+'</td><td>'+str(d)+'</td><td>'+str(dictOfWords)+'</td><td><img src="data:image/jpeg;base64,'+base64img+'"></td></tr></table></body></html>'
+    strhtml='<html><head><style>  body{background-color:RosyBrown;}#customers {font-family: "Lucida Console", Monaco, monospace;border-collapse: collapse;border-radius: 2em;overflow: hidden;width:80%;height:45%;margin-top: 150px;margin-right: 150px;margin-left:150px;}#customers td, #customers th {border: 1px solid #ddd;padding: 8px;}#customers tr:nth-child(even){background-color: LavenderBlush;}#customers tr:hover {background-color: #ddd;}#customers th {padding-top: 12px;padding-bottom: 12px;text-align: left;background-color: DeepSkyBlue;color: white;}</style></head><body><h3><center>Threshold set: '+str(threshold)+' Error calculated:'  +str(_mse)+ '</center></h3><p></p><table id="customers" ><tr><th>PATIENT_NAME</th><th>PATIENT_ID</th><th>IMAGE_NAME</th><th>PREDICTED_CLASS</th><th>PREDICTIONS</th><th>IMAGE</th></tr><tr><td>'+names+'</td><td>'+ids+'</td><td>'+a[5:]+'</td><td>'+str(d)+'</td><td>'+str(dictOfWords)+'</td><td><img src="data:image/jpeg;base64,'+base64img+'"></td></tr></table></body></html>'
 
      
 
     return strhtml
     
 
-
+#Method detects of uploaded image is not the breast memography
+def IsImageHasAnomaly(autoencoder, filePath,threshold):  
+    im = cv2.resize(cv2.imread(filePath), (996, 996))
+    im = im * 1./255
+    datas = np.zeros((1,  996, 996, 3))
+    validation_image = np.zeros((1,  996, 996, 3),np.float32)
+    validation_image[0, :, :, :] = im;   
+    #print(validation_image[0].shape)
+    predicted_image = autoencoder.predict(validation_image)
+    #predicted_image = predicted_image.astype(np.float32)
+    #validation_image = cv2.cvtColor(validation_image[0], cv2.COLOR_BGR2GRAY)
+    #predicted_image = cv2.cvtColor(predicted_image[0], cv2.COLOR_BGR2GRAY)
+    #validation_image = tf.image.rgb_to_grayscale(validation_image, name=None)
+    #predicted_image = tf.image.rgb_to_grayscale(predicted_image, name=None)
+    #print(validation_image.shape)
+    #mssim,score = compare_ssim(predicted_image, validation_image,full=True,multichannel=True) 
+    _mse = mse(predicted_image[0], validation_image[0]) 
+    #print(_mse)
+    #print(score)
+    
+    app.logger.info('_mse: {}'.format(_mse))
+    return _mse  > threshold , round(_mse, 4) , threshold
+    
+    
+def mse(imageA, imageB):
+    err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+    err /= float(imageA.shape[0] * imageA.shape[1])
+    return err
+    
+def anamolyDetector(inputImage):
+    img_width, img_height = 996, 996
+    input_img = Input(batch_shape=(None, img_width, img_width, 3))
+    x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
+    x = MaxPooling2D((2, 2), padding='same')(x)
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    x = MaxPooling2D((2, 2), padding='same')(x)
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    encoded = MaxPooling2D((2, 2), padding='same')(x)
+    
+    # at this point the representation is (4, 4, 8) i.e. 128-dimensional
+    
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
+    x = UpSampling2D((2, 2))(x)
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    x = UpSampling2D((2, 2))(x)
+    x = Conv2D(16, (3, 3), activation='relu')(x)
+    x = UpSampling2D((2, 2))(x)
+    decoded = Conv2D(3, (3, 3), activation='sigmoid', padding='same')(x)
+    
+    autoencoder = Model(input_img, decoded)
+    
+    autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+    
+    #autoencoder.summary()
+    autoencoder.load_weights('trained_models/anomaly-detection.h5');    
+    threshold=0.020
+    isAnamoly , _mse , threshold = IsImageHasAnomaly(autoencoder, inputImage,threshold)
+    
+    return isAnamoly , _mse , threshold
     
 '''train_generator = train_datagenerator(test_batchsize)
 print('summary of loaded model')
